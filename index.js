2bc88f953ed407079b01bacb791a378663ecdb17
Mujkauser <126848798+Mujkauser@users.noreply.github.com>
2025-09-30 19:09:12 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,17 @@
-// index.js
 const express = require("express");
 const { getRedditPosts } = require("./redditFetcher");
+
 const app = express();
 const PORT = process.env.PORT || 10000;
 
 app.set("view engine", "ejs");
 app.set("views", __dirname + "/views");
 
+// Home route
 app.get("/", async (req, res) => {
-  const redditPosts = await getRedditPosts("FrontFaith74"); // full posts
-  res.render("index", { redditPosts }); // pass to main template
+  const redditPosts = await getRedditPosts("FrontFaith74");
+
+  res.render("redditPosts", { redditPosts });
 });
 
 app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
-
